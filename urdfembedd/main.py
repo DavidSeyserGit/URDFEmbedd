81ae06dc98e42919604a8b3d228dda3461a3acfa
DavidSeyserGit <david.seyser@hotmail.com>
2025-09-03 16:15:35 +0200

    Refactor code structure for improved readability and maintainability

--- urdfembedd/main.py
+++ urdfembedd/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from urdfembedd!")
+import torch
+from torchvision import datasets
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from autoencoder import Autoenc
 
+def main():
+    model = Autoenc()
+    model.test()
 
 if __name__ == "__main__":
     main()
+ 
